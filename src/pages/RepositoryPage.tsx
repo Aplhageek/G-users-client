50a40cccd29d7e233fee7b2641fd543b004b4503
Jayesh <en001507@gmail.com>
2024-07-10 18:07:04 +0530

    added Home btn

--- src/pages/RepositoryPage.tsx
+++ src/pages/RepositoryPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { Repository, User } from './Home';
 import UserCard from '../components/UserCard/UserCard';
 
@@ -8,7 +8,15 @@ const RepositoryPage: React.FC = () => {
     const repo = location.state?.repo as Repository;
     const user = location.state?.owner as User;
 
+    const navigate = useNavigate();
+
+    const goHome = () => {
+        navigate("/");
+    };
+
     return (
+        <>
+        <div className="homebtn" onClick={goHome}>Home</div>
         <div className='repoPageWrapper'>
             <UserCard user={user} />
             <div className="userCard">
@@ -21,6 +29,7 @@ const RepositoryPage: React.FC = () => {
                 </div>
             </div>
         </div>
+        </>
     );
 };
 
